d89ca2bd705e442ccc59732595e119b2bf903c42
mattkersner <mattkersner@gmail.com>
2018-10-27 22:19:46 -0400

    recongifugred app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var request = require('request');
 var slackbot = require('./slackbot-new');
 var fs = require('fs');
 
-var botKey = "xoxb-357081308928-5iUqqrPfPlT8Xv2gtoEf19oW";
+var botKey = "xoxb-2686276489-465948777747-HkAErDMguD5nRY3Vo3OEyDwY";
 var taIDs = "U361Q3GPP,U2JU26MPS,U42S89JCX,U2BGW8TS7,U3K0RFJAY,U5Q1G6L91,U4A9PV49F".split(",");
 
 var bot = new slackbot(botKey);
